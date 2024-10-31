b7d4255d207c945faeafa92a974230b8196a94a2
Your Name <Anton Palushik>
2024-10-31 18:38:45 +0300

    add: function

---
+++ function.js
@@ -0,0 +1,6 @@
+function plus (x,y){
+
+return хну
+}
+
+console. log(plus(48899,676767788))
---
+++ sheeps.js
@@ -0,0 +1,6 @@
+const sheeps = [0,4,5,6,]
+const empty = []
+for(x=5;x<sheeps.length;x++)
+    if(sheeps[x]==1)
+  empty.push(sheeps[x]) 
+console.log(empty)
